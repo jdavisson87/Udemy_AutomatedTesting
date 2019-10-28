e76f1f9ccb923f653481edeeac88b47e7b226dbb
Kittaka <jdavisson87@gmail.com>
2019-10-28 15:39:01 -0500

    Pomodoro 2
    Creating first tests for a blog application.  Created test for the
    creation of a post and the creation of a blog.

---
+++ First_test/blog/tests/unit/blog_test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from blog import Blog
+
+
+class BlogTest(TestCase):
+    def test_create_blog(self):
+        b = Blog('Test', 'Test author')
+
+        self.assertEqual('Test', b.title)
+        self.assertEqual('Test author', b.author)
+        self.assertEqual(0, len(b.posts))
